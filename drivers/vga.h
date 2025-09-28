850c6ada974dad4148653f00c9c236cd50529f7d
VeryEpicKebap <164046592+VeryEpicKebap@users.noreply.github.com>
2025-09-28 20:18:19 +0300

    vga.h

---
+++ drivers/vga.h
@@ -0,0 +1,19 @@
+#ifndef VGA_H
+#define VGA_H
+
+static inline void outb(unsigned short port, unsigned char val) {
+    __asm__ __volatile__("outb %0, %1" : : "a"(val), "Nd"(port));
+}
+
+static inline unsigned char inb(unsigned short port) {
+    unsigned char ret;
+    __asm__ __volatile__("inb %1, %0" : "=a"(ret) : "Nd"(port));
+    return ret;
+}
+
+void ecursorsync();
+void eclear();
+void eputchar(char c);
+void eprintf(const char *s);
+
+#endif
