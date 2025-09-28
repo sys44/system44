a1385356727af3c97f75c8896c212a62916b6eee
VeryEpicKebap <164046592+VeryEpicKebap@users.noreply.github.com>
2025-09-28 14:29:53 +0300

    ...

---
+++ build.sh
@@ -0,0 +1,13 @@
+#!/bin/bash
+
+CFLAGS="-B/usr/lib/gcc/i686-elf/15.1.0 -m32 -ffreestanding -nostdlib -fno-pie -fno-stack-protector -O2 -Wall -Wextra -Wno-unused-parameter"
+LDFLAGS="-m elf_i386 -Ttext 0x10000 -e _start"
+nasm boot/boot.s -f bin -o boot.bin
+nasm kernel/entry.s -f elf -o entry.o
+i686-elf-gcc $CFLAGS -c kernel/init.c -o kernel.o
+i686-elf-ld $LDFLAGS -o kernel.elf \
+    entry.o \
+    kernel.o \
+i686-elf-objcopy -O binary kernel.elf kernel.bin
+cat boot.bin kernel.bin > floppy.img
+
