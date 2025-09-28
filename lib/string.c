f7616bec672aba7cbe2020e996bed1a20d8d5b0c
VeryEpicKebap <164046592+VeryEpicKebap@users.noreply.github.com>
2025-09-28 20:21:52 +0300

    string.c

---
+++ lib/string.c
@@ -0,0 +1,29 @@
+#include "string.h"
+
+size_t strlen(const char *s) {
+    size_t len = 0;
+    while (*s++) len++;
+    return len;
+}
+
+int strcmp(const char *s1, const char *s2) {
+    while (*s1 && (*s1 == *s2)) {
+        s1++;
+        s2++;
+    }
+    return *(unsigned char*)s1 - *(unsigned char*)s2;
+}
+
+char *strcpy(char *dest, const char *src) {
+    char *d = dest;
+    while ((*d++ = *src++));
+    return dest;
+}
+
+char *strncpy(char *dest, const char *src, size_t n) {
+    char *d = dest;
+    size_t i = 0;
+    for (; i < n && src[i]; i++) d[i] = src[i];
+    for (; i < n; i++) d[i] = 0;
+    return dest;
+}
