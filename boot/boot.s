d4da3b92ce95b187d6ce73579c7900f9d89f4a03
VeryEpicKebap <164046592+VeryEpicKebap@users.noreply.github.com>
2025-09-28 14:32:22 +0300

    bootloader update

--- boot/boot.s
+++ boot/boot.s
@@ -1,35 +1,27 @@
 [org 0x7c00]
-KERNEL_OFFSET EQU 0x1000
 
 BOOT_DRIVE db 0x0
-
-MSG_REAL_MODE: db "started in 16-bit real mode", 0xa, 0xd, 0x0
-MSG_LOAD_KERNEL: db "loading kernel into memory...", 0x0
-MSG_PROT_MODE: db "successfully landed in 32-bit protected mode.", 0x0
-
+ERROR_MSG: db 'disk error', 0
 mov [BOOT_DRIVE], dl
 mov bp, 0x9000
 mov sp, bp
-mov bx, MSG_REAL_MODE
-call print_string
-call load_kernel
-call switch_to_pm
+call loadk
+call pms
 jmp $
 
-print_string:
+printstr:
   pusha
   mov ah, 0xe
-  jmp read_character
-read_character:
+.loop:
   mov al, [bx]
   cmp al, 0
-  jne print_character
-  popa
-  ret
-print_character:
+  je .done
   int 0x10
   add bx, 1
-  jmp read_character
+  jmp .loop
+.done:
+  popa
+  ret
 
 disk_load:
   pusha
@@ -48,9 +40,8 @@ disk_load:
   ret
 disk_error:
   mov bx, ERROR_MSG
-  call print_string
+  call printstr
   jmp $
-ERROR_MSG: db 'disk error', 0
 
 gdt_start:
 gdt_null:
@@ -70,7 +61,6 @@ gdt_data:
   db 10010010b
   db 11001111b
   db 0x0
-
 gdt_end:
 
 gdt_descriptor:
@@ -81,7 +71,17 @@ CODE_SEG EQU gdt_code - gdt_start
 DATA_SEG EQU gdt_data - gdt_start
 
 [bits 16]
-switch_to_pm:
+loadk:
+  mov ax, 0x1000
+  mov es, ax
+  mov bx, 0x0000
+  mov dh, 47
+  mov dl, [BOOT_DRIVE]
+  call disk_load
+  ret
+
+[bits 16]
+pms:
   cli
   lgdt [gdt_descriptor]
   mov eax, cr0
@@ -101,39 +101,9 @@ init_pm:
   mov esp, ebp
   call BEGIN_PM
 
-VIDEO_MEMORY equ 0xb8000
-WHITE_ON_BLACK equ 0x0f
-print_string_pm:
-  pusha
-  mov edx, VIDEO_MEMORY
-print_string_pm_loop:
-  mov al, [ebx]
-  mov ah, WHITE_ON_BLACK
-  cmp al, 0
-  je print_string_pm_end
-  mov [edx], ax
-  add ebx, 1
-  add edx, 2
-  jmp print_string_pm_loop
-print_string_pm_end:
-  popa
-  ret
-
-[bits 16]
-load_kernel:
-  mov bx, MSG_LOAD_KERNEL
-  call print_string
-  mov bx, KERNEL_OFFSET
-  mov dh, 47
-  mov dl, [BOOT_DRIVE]
-  call disk_load
-  ret
-
 [bits 32]
 BEGIN_PM:
-  mov ebx, MSG_PROT_MODE
-  call print_string_pm
-  call KERNEL_OFFSET
+  call 0x10000
   jmp $
 
 times 510-($-$$) db 0x0
