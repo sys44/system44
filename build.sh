ae59fc15f7c5166a4e226d7b21aef06169d612be
VeryEpicKebap <164046592+VeryEpicKebap@users.noreply.github.com>
2025-09-28 14:32:44 +0300

    quick fix

--- build.sh
+++ build.sh
@@ -5,9 +5,11 @@ LDFLAGS="-m elf_i386 -Ttext 0x10000 -e _start"
 nasm boot/boot.s -f bin -o boot.bin
 nasm kernel/entry.s -f elf -o entry.o
 i686-elf-gcc $CFLAGS -c kernel/init.c -o kernel.o
+
 i686-elf-ld $LDFLAGS -o kernel.elf \
     entry.o \
     kernel.o \
+
 i686-elf-objcopy -O binary kernel.elf kernel.bin
 cat boot.bin kernel.bin > floppy.img
 
