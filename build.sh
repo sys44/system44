0ff00b18cd892656bca92a9064320586d026a0bb
kernelfucker <kernelfucker@tutanota.com>
2025-10-08 11:42:45 -0500

    a few changes in build.sh

--- build.sh
+++ build.sh
@@ -1,6 +1,8 @@
 #!/bin/sh
 
-CFLAGS="-B/usr/lib/gcc/i686-elf/15.1.0 -m32 -ffreestanding -nostdlib -fno-pie -fno-stack-protector -O2 -Wall -Wextra -Wno-unused-parameter"
+CC=tcc
+
+CFLAGS="-m32 -ffreestanding -nostdlib -fno-pie -fno-stack-protector -O2 -Wall -Wextra -Wno-unused-parameter"
 LDFLAGS="-m elf_i386 -Ttext 0x10000 -e _start"
 
 echo "assemble: boot/boot.s"
@@ -10,40 +12,40 @@ echo "assemble: kernel/entry.s"
 nasm kernel/entry.s -f elf -o entry.o
 
 echo "compile: kernel/init.c"
-i686-elf-gcc $CFLAGS -c kernel/init.c -o init.o
+$CC $CFLAGS -c kernel/init.c -o init.o
 
 echo "compile: kernel/shell.c"
-i686-elf-gcc $CFLAGS -c kernel/shell.c -o shell.o
+$CC $CFLAGS -c kernel/shell.c -o shell.o
 
 echo "compile: drivers/vga.c"
-i686-elf-gcc $CFLAGS -c drivers/vga.c -o vga.o
+$CC $CFLAGS -c drivers/vga.c -o vga.o
 
 echo "compile: drivers/keyboard.c"
-i686-elf-gcc $CFLAGS -c drivers/keyboard.c -o keyboard.o
+$CC $CFLAGS -c drivers/keyboard.c -o keyboard.o
 
 echo "compile: drivers/tty.c"
-i686-elf-gcc $CFLAGS -c drivers/tty.c -o tty.o
+$CC $CFLAGS -c drivers/tty.c -o tty.o
 
 echo "compile: drivers/ata.c"
-i686-elf-gcc $CFLAGS -c drivers/ata.c -o ata.o
+$CC $CFLAGS -c drivers/ata.c -o ata.o
 
 echo "compile: lib/memory.c"
-i686-elf-gcc $CFLAGS -c lib/memory.c -o memory.o
+$CC $CFLAGS -c lib/memory.c -o memory.o
 
 echo "compile: lib/string.c"
-i686-elf-gcc $CFLAGS -c lib/string.c -o string.o
+$CC $CFLAGS -c lib/string.c -o string.o
 
 echo "compile: mm/lmm.c"
-i686-elf-gcc $CFLAGS -c mm/lmm.c -o lmm.o
+$CC $CFLAGS -c mm/lmm.c -o lmm.o
 
 echo "compile: mm/pmm.c"
-i686-elf-gcc $CFLAGS -c mm/pmm.c -o pmm.o
+$CC $CFLAGS -c mm/pmm.c -o pmm.o
 
 echo "compile: mm/paging.c"
-i686-elf-gcc $CFLAGS -c mm/paging.c -o paging.o
+$CC $CFLAGS -c mm/paging.c -o paging.o
 
 echo "linking files.."
-i686-elf-ld $LDFLAGS -o kernel.elf \
+ld $LDFLAGS -o kernel.elf \
     entry.o \
     init.o \
     shell.o \
@@ -57,7 +59,7 @@ i686-elf-ld $LDFLAGS -o kernel.elf \
     paging.o
 
 echo "objcopy: kernel.elf"
-i686-elf-objcopy -O binary kernel.elf kernel.bin
+objcopy -O binary kernel.elf kernel.bin
 
 echo "creating final image.."
 cat boot.bin kernel.bin > floppy.img
