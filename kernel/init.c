49f6dab657dba44e9ad3c8a0a49e4f02817f740f
Abdullah ebn Oliullah <Oliullah321123@protonmail.com>
2025-10-18 16:35:57 +0600

    test in init

--- kernel/init.c
+++ kernel/init.c
@@ -14,6 +14,30 @@ This is the first part of the kernel ran when the kernel actually starts executi
 #include "../int/interrupts.h"
 #include "log.h"
 
+void tirq0(void) {
+    /* Reduced to 10 for faster boot */
+    tty_puts("\n[ 0.0000 ] running IRQ0 (timer) test.\n");
+    uint32_t last_tick = ticks;
+    while (ticks < 10) {
+        if (ticks != last_tick) {
+            last_tick = ticks;
+            tty_puts("*");
+        }
+    }
+    tty_puts("\n[ 0.0000 ] 10 ticks elapsed\n");
+}
+
+static inline uint32_t syscall3(uint32_t num, uint32_t arg1, uint32_t arg2, uint32_t arg3) {
+    uint32_t ret;
+    asm volatile(
+        "int $0x80"
+        : "=a"(ret)
+        : "a"(num), "b"(arg1), "c"(arg2), "d"(arg3)
+        : "memory"
+    );
+    return ret;
+}
+
 void kmain(unsigned char *vbe){
     tty_init(vbe);
     tty_puts("\n - system44 v");
@@ -26,6 +50,10 @@ void kmain(unsigned char *vbe){
     asm volatile("sti");
     mmp();
     pmm_init();
+    tirq0();
+    tty_puts("[ 0.0000 ] testing syscalls\n");
+    syscall0(0);  // test syscall with no args
+    uint32_t ret = syscall3(1, 0x1234, 0, 0);  // test with args
     kfs_mount();
     sh();
 }
